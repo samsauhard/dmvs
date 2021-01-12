1c97b6c589f3964fb5b5c9e88c83110cde24dc81
Sauhard <43118859+samsauhard@users.noreply.github.com>
2021-01-12 15:19:44 -0500

    Update example_test.py

--- example_test.py
+++ example_test.py
@@ -43,7 +43,7 @@ def search_and_click(ua,sleep_time,top5,proxy,proxies,sleep_after):
     options1.binary_location = GOOGLE_CHROME_PATH
     #options1.add_argument('user-agent=%s'%ua.random)
 
-    driver = webdriver.Chrome(options=options1)
+    driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH,options=options1)
     #options.add_experimental_option( "prefs",{'profile.managed_default_content_settings.javascript': 2})
 
     
