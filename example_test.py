a61bec5dc88dff5b18b7a2bc16963f98457544f9
Sauhard <43118859+samsauhard@users.noreply.github.com>
2021-01-16 03:18:59 -0500

    Update example_test.py
    xz

--- example_test.py
+++ example_test.py
@@ -52,7 +52,7 @@ def search_and_click(ua,sleep_time,top5,proxy,proxies,sleep_after):
     
         #driver.get('https://www.dailymotion.com/video/x7yfnh3')
     try:  
-        driver.get('https://www.dailymotion.com/video/x7ymzna')
+        driver.get('https://dai.ly/x7ymznr')
         #time.sleep(3)
         #mm = driver.find_element_by_class_name('top-matter')
         #mm = driver.find_element_by_class_name('md')
